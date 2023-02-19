1c3121a399abf58fb140bc36cc946deff97a7956
DarkB0B <miecze123@onet.pl>
2023-02-19 01:07:57 +0100

    Get All Free On Given DAte

--- API Prototype ASP/Controllers/BookingController.cs
+++ API Prototype ASP/Controllers/BookingController.cs
@@ -19,11 +19,10 @@ namespace API_Prototype_ASP.Controllers
         [HttpPost]
         public JsonResult Create(Reservation reservation)
         {
-            if (reservation == null || reservation.RoomNumber == 0 || reservation.ClientName == null || reservation.StartDate == null || reservation.EndDate == null || reservation.StartDate > reservation.EndDate || reservation.StartDate > DateTime.Now)
+            if (reservation == null || reservation.RoomNumber == 0 || reservation.RoomNumber < 5 || reservation.ClientName == null || reservation.StartDate == null || reservation.EndDate == null || reservation.StartDate > reservation.EndDate || reservation.StartDate > DateTime.Now)
             {
                 return new JsonResult("Invalid reservation");
             }
-            
             
             var reservationsForThisRoom = _context.Reservations.Where(r => (r.RoomNumber == reservation.RoomNumber) && (reservation.StartDate >= DateTime.Now)).ToList();
             foreach (var res in reservationsForThisRoom)
@@ -44,13 +43,27 @@ namespace API_Prototype_ASP.Controllers
                 return new JsonResult(Ok(reservation));
 
         }
-        
+        //Get all reservations
         [HttpGet]
-        public JsonResult Get()
+        public JsonResult GetAll()
         {
             return new JsonResult(_context.Reservations.ToList());
         }
-
+        //Get all free rooms on given date
+        [HttpGet("free/{date}")]
+        public JsonResult GetFreeRooms(DateTime date)
+        {
+            var reservations = _context.Reservations.Where(r => r.StartDate <= date && r.EndDate >= date).ToList();
+            var freeRooms = new List<int>();
+            for (int i = 1; i <= 4; i++)
+            {
+                if (!reservations.Any(r => r.RoomNumber == i))
+                {
+                    freeRooms.Add(i);
+                }
+            }
+            return new JsonResult(freeRooms);
+        }
 
 
     }
