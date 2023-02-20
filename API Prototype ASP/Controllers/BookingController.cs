9df8e394a07311f795c73383831b9f13e472226a
DarkB0B <miecze123@onet.pl>
2023-02-20 01:53:38 +0100

    Made code more "RESTFUL"

--- API Prototype ASP/Controllers/BookingController.cs
+++ API Prototype ASP/Controllers/BookingController.cs
@@ -36,15 +36,20 @@ namespace API_Prototype_ASP.Controllers
             _context = context;
         }
         // Create
-        [AllowAnonymous]
-        [HttpPost]
-        public JsonResult Create(Reservation reservation)
+        [HttpPost("book/{roomno}/{startDate}/{endDate}")]
+        [Authorize]
+        public JsonResult Create(int roomno, DateTime startDate, DateTime endDate)
         {
+            Reservation reservation = new Reservation();
+            reservation.RoomNumber = roomno;
+            reservation.StartDate = startDate;
+            reservation.EndDate = endDate;
+
             if (reservation == null || reservation.RoomNumber == 0 || reservation.RoomNumber > 5 || reservation.ClientName == null || reservation.StartDate == null || reservation.EndDate == null || reservation.StartDate > reservation.EndDate || reservation.StartDate < DateTime.Now)
             {
                 return new JsonResult("Invalid reservation");
             }
-            
+            var currentuser = GetCurrentUser();
             var reservationsForThisRoom = _context.Reservations.Where(r => (r.RoomNumber == reservation.RoomNumber) && (reservation.StartDate >= DateTime.Now)).ToList();
             foreach (var res in reservationsForThisRoom)
             {
@@ -100,8 +105,30 @@ namespace API_Prototype_ASP.Controllers
             }
             return new JsonResult(freeRooms);
         }
-
-
+        //Get list of reservations for room
+        [HttpGet("room/{roomno}")]
+        [Authorize]
+        public JsonResult GetRoomReservations(int roomno)
+        {
+            var reservations = _context.Reservations.Where(r => r.RoomNumber == roomno && r.StartDate > DateTime.Now).ToList();
+            if (reservations.Count == 0)
+            {
+                return new JsonResult("No reservations found for this room");
+            }
+            return new JsonResult(reservations);
+        }
+        //Get list of reservations for time period 
+        [HttpGet("period/{startDate}/{endDate}")]
+        [Authorize]
+        public JsonResult GetPeriodReservations(DateTime startDate, DateTime endDate)
+        {
+            var reservations = _context.Reservations.Where(r => r.StartDate >= startDate && r.EndDate <= endDate).ToList();
+            if (reservations.Count == 0)
+            {
+                return new JsonResult("No reservations found for this period");
+            }
+            return new JsonResult(reservations);
+        }
 
     }
 }
